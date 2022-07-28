0514285fe1b422242764f464d5f6db62bb21eb92
ernberna <ernesto.bernabe@digitas.com>
2022-07-28 11:13:39 -0600

    post db config

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,14 @@
-const store = require('../../../store/mysql');
+const config = require('../../../config');
+let store;
+// const store = require('../../../store/mysql');
 //const store = require('../../../store/remote-mysql');
+if (config.remoteDB === true) {
+    store = require('../../../store/remote-mysql');
+} else {
+    store = require('../../../store/mysql');
+}
+
+
 const ctrl = require('./controller');
 
 module.exports = ctrl(store);
